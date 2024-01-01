436f3987cea7705e12fa9ce351a45e02f794206e
NguyenLe1605 <lhdnguyen2002@gmail.com>
2024-01-01 13:41:41 +0700

    Add variable integer encoding

--- src/varint.rs
+++ src/varint.rs
@@ -1,3 +1,5 @@
+
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum VarInt {
     U8(u8),
     U16(u16),
@@ -27,10 +29,52 @@ impl VarInt {
             num => Self::Unknown(num),
         }
     }
+
+    fn encode_bytes(dst: &mut [u8], src: &[u8], mask: u8) {
+        if dst.len() < src.len() {
+            todo!("Add proper error handling");
+        }
+        
+        dst.copy_from_slice(src);
+        dst[0] |= mask
+    }
+
+    pub fn encode(&self, bytes: &mut [u8]) {
+        match self {
+            Self::Unknown(_) => unreachable!(),
+            Self::U8(num) => VarInt::encode_bytes(bytes, &num.to_be_bytes()[..], 0x00),
+            Self::U16(num) => VarInt::encode_bytes(bytes, &num.to_be_bytes()[..], 0x40),
+            Self::U32(num) => VarInt::encode_bytes(bytes, &num.to_be_bytes()[..], 0x80),
+            Self::U64(num) => VarInt::encode_bytes(bytes, &num.to_be_bytes()[..], 0xc0),
+        } 
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::varint::VarInt;
+
     #[test]
-    fn decode_nums() {}
+    fn decode_variable_integers() {
+        assert_eq!(VarInt::decode(&0xc2197c5eff14e88cu64.to_be_bytes()), VarInt::U64(151_288_809_941_952_652));
+        assert_eq!(VarInt::decode(&0x9d7f3e7du32.to_be_bytes()), VarInt::U32(494_878_333));
+        assert_eq!(VarInt::decode(&0x7bbdu16.to_be_bytes()), VarInt::U16(15_293));
+        assert_eq!(VarInt::decode(&0x25u8.to_be_bytes()), VarInt::U8(37));
+        assert_eq!(VarInt::decode(&0x4025u16.to_be_bytes()), VarInt::U16(37));
+    }
+
+    #[test]
+    fn encode_variable_integers() {
+        let tc = vec![
+            (VarInt::U64(151_288_809_941_952_652), 8),
+            (VarInt::U32(494_878_333), 4),
+            (VarInt::U16(15_293), 2),
+            (VarInt::U8(32), 1),
+        ];
+        for (num, size) in tc.into_iter() {
+            let mut buf = vec![0u8; size];
+            num.encode(&mut buf);
+            assert_eq!(VarInt::decode(&buf), num);
+        }
+    }
 }
